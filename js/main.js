23a9e8693c72589732638a908ca56513af57510a
Marcus Lima <96790465+marcuslima96@users.noreply.github.com>
2026-01-30 15:51:17 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -39,12 +39,7 @@ const LANE_MAPPING = {
         'Yasuo', 'Yunara', 'Zeri', 'Ziggs'
     ],
     support: [
-        'Alistar', 'Ashe', 'Bard', 'Blitzcrank', 'Brand', 'Braum', 'Elise', 
-        'Fiddlesticks', 'Janna', 'Karma', 'Leblanc', 'Leona', 'Lulu', 'Lux', 
-        'Maokai', 'Mel', 'Milio', 'Morgana', 'Nami', 'Nautilus', 'Neeko', 
-        'Pantheon', 'Poppy', 'Pyke', 'Rakan', 'Rell', 'Renata', 'Senna', 
-        'Seraphine', 'Shaco', 'Sona', 'Soraka', 'Swain', 'TahmKench', 'Taric', 
-        'Thresh', 'Velkoz', 'Xerath', 'Yuumi', 'Zilean', 'Zoe', 'Zyra'
+        'Alistar'
     ]
 };
 
